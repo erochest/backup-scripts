0fb6076ee2fd0789c86ec57ec2bbe99cdfc3790e
Eric Rochester <erochest@virginia.edu>
2014-09-09 07:10:44 -0400

    rm-dups

--- RmDups.hs
+++ RmDups.hs
@@ -1,8 +1,74 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE RecordWildCards      #-}
 
 
 module Main where
 
 
+import           Control.Monad
+import           Data.Attoparsec.Text
+import qualified Data.Text                 as T
+import           Filesystem.Path.CurrentOS
+import           Options.Applicative       hiding (Parser)
+import qualified Options.Applicative       as A
+import           Prelude                   hiding (FilePath)
+import           Shelly
+
+
+default (T.Text)
+
+
 main :: IO ()
-main = undefined
+main = do
+    RmDups{..} <- A.execParser opts
+    shelly $ verbosely $ do
+        dups <-  foldr (liftA2 (:) . parseOnly duplicate) (Right []) . T.lines
+             <$> readfile dupFile
+        case dups of
+            Left err    -> liftIO . putStrLn $ "ERROR: " ++ err
+            Right dups' -> forM_ dups' $ \(primary, dupFiles) -> do
+                echo $  "KEEPING " <> toTextIgnore primary
+                     <> ": deleting " <> tshow (length dupFiles)
+                mapM_ rm_f dupFiles
+
+-- Utilities
+
+tshow :: Show a => a -> T.Text
+tshow = T.pack . show
+
+-- Parsing
+
+type Duplicate = (FilePath, [FilePath])
+
+duplicate :: Parser Duplicate
+duplicate = (,) <$> (filepath <* space) <*> sepBy1 filepath space
+
+filepath :: Parser FilePath
+filepath = decodeString <$> many1 (escaped <|> notChar ' ')
+
+escaped :: Parser Char
+escaped = char '\\' *> anyChar
+
+-- Command line
+
+data RmDups = RmDups
+            { baseDir :: FilePath
+            , dupFile :: FilePath
+            } deriving (Show)
+
+opts' :: A.Parser RmDups
+opts' =   RmDups
+      <$> fileOption (  short 'd' <> long "dir" <> metavar "DIR_NAME"
+                     <> help "The base directory to process everything from.")
+      <*> fileOption (  short 'i' <> long "input" <> metavar "DUP_FILE"
+                     <> help "The file listing duplicates.")
+
+opts :: A.ParserInfo RmDups
+opts = info (helper <*> opts')
+            (  fullDesc
+            <> progDesc "Removes duplicate files from the output of fdups."
+            <> header "rm-dups")
+
+fileOption :: Mod OptionFields FilePath -> A.Parser FilePath
+fileOption = A.option (pure . decodeString)
