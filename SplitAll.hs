407fb4570032f3a3949728b6e7ddbc084a080e5d
Eric Rochester <erochest@virginia.edu>
2014-10-02 09:38:08 -0400

    Only attempt to split files that are large enough.

--- SplitAll.hs
+++ SplitAll.hs
@@ -5,10 +5,11 @@
 module Main where
 
 
-import           Control.Applicative
+import           Control.Monad
 import qualified Data.List                 as L
 import           Data.Monoid
 import qualified Data.Text                 as T
+import qualified Filesystem                as FS
 import           Filesystem.Path.CurrentOS hiding ((</>))
 import           Prelude                   hiding (FilePath)
 import           Shelly
@@ -22,9 +23,16 @@ toBoxDir = "/Volumes/Untitled/to-box"
 splitSize :: T.Text
 splitSize = "4096m"
 
+-- | This should be something smaller than 4m, which is above.
+maxWholeSize :: Integer
+maxWholeSize = 3 * 2^(30 :: Integer)
+
 main :: IO ()
 main = shelly $ verbosely $ chdir toBoxDir $
-    mapM_ splitFile =<< L.sort . filter (hasExt "xz") <$> ls toBoxDir
+        ls toBoxDir
+    >>= filterM (liftIO . fmap (>= maxWholeSize) . FS.getSize)
+    .   filter (hasExt "xz")
+    >>= mapM_ splitFile . L.sort
 
 splitFile :: FilePath -> Sh ()
 splitFile fn = do
